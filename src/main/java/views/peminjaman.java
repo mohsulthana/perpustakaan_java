1331d5c115974d780baf77c6080bf05704b73b25
KeikoATR72 <58618886+KeikoATR72@users.noreply.github.com>
2019-12-08 12:15:53 +0700

    Added status and borrowing date

--- src/main/java/views/peminjaman.java
+++ src/main/java/views/peminjaman.java
@@ -6,6 +6,7 @@
 package views;
 
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import model.modelPeminjaman;
@@ -19,6 +20,7 @@ public class peminjaman extends javax.swing.JFrame {
  
     public peminjaman() throws SQLException {
         initComponents();
+        
     }
 
     /**
@@ -33,6 +35,12 @@ public class peminjaman extends javax.swing.JFrame {
         buttonGroup1 = new javax.swing.ButtonGroup();
         dateComponentFormatter1 = new org.jdatepicker.impl.DateComponentFormatter();
         buttonGroup2 = new javax.swing.ButtonGroup();
+        dateComponentFormatter2 = new org.jdatepicker.impl.DateComponentFormatter();
+        dateComponentFormatter3 = new org.jdatepicker.impl.DateComponentFormatter();
+        jDatePickerUtil1 = new org.jdatepicker.util.JDatePickerUtil();
+        utilCalendarModel1 = new org.jdatepicker.impl.UtilCalendarModel();
+        jDateComponentFactory1 = new org.jdatepicker.JDateComponentFactory();
+        jDatePickerUtil2 = new org.jdatepicker.util.JDatePickerUtil();
         jPanel1 = new javax.swing.JPanel();
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
@@ -46,6 +54,9 @@ public class peminjaman extends javax.swing.JFrame {
         jLabel4 = new javax.swing.JLabel();
         comboBoxBuku = new javax.swing.JComboBox<>();
         jLabel5 = new javax.swing.JLabel();
+        dipinjam = new javax.swing.JRadioButton();
+        dikembalikan = new javax.swing.JRadioButton();
+        tanggal = new com.toedter.calendar.JDateChooser();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -85,8 +96,29 @@ public class peminjaman extends javax.swing.JFrame {
         jLabel4.setText("Status");
 
         comboBoxBuku.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        comboBoxBuku.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                comboBoxBukuActionPerformed(evt);
+            }
+        });
 
         jLabel5.setText("Buku");
+
+        buttonGroup1.add(dipinjam);
+        dipinjam.setText("Dipinjam");
+        dipinjam.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                dipinjamActionPerformed(evt);
+            }
+        });
+
+        buttonGroup1.add(dikembalikan);
+        dikembalikan.setText("Dikembalikan");
+        dikembalikan.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                dikembalikanActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -106,13 +138,17 @@ public class peminjaman extends javax.swing.JFrame {
                                     .addComponent(jLabel3))
                                 .addGap(41, 41, 41)
                                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                        .addComponent(comboBoxSiswa, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                        .addComponent(comboBoxBuku, javax.swing.GroupLayout.PREFERRED_SIZE, 239, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                    .addComponent(comboBoxSiswa, javax.swing.GroupLayout.PREFERRED_SIZE, 345, javax.swing.GroupLayout.PREFERRED_SIZE)
                                     .addGroup(jPanel1Layout.createSequentialGroup()
                                         .addComponent(txtLamaPinjam, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                        .addComponent(jLabel2))
+                                    .addGroup(jPanel1Layout.createSequentialGroup()
+                                        .addComponent(dipinjam, javax.swing.GroupLayout.PREFERRED_SIZE, 87, javax.swing.GroupLayout.PREFERRED_SIZE)
                                         .addGap(18, 18, 18)
-                                        .addComponent(jLabel2))))
+                                        .addComponent(dikembalikan, javax.swing.GroupLayout.PREFERRED_SIZE, 87, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                    .addComponent(comboBoxBuku, javax.swing.GroupLayout.PREFERRED_SIZE, 239, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(tanggal, javax.swing.GroupLayout.PREFERRED_SIZE, 239, javax.swing.GroupLayout.PREFERRED_SIZE)))
                             .addGroup(jPanel1Layout.createSequentialGroup()
                                 .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 109, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addGap(18, 18, 18)
@@ -121,14 +157,14 @@ public class peminjaman extends javax.swing.JFrame {
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGap(134, 134, 134)
                         .addComponent(jLabel6)))
-                .addContainerGap(54, Short.MAX_VALUE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGap(44, 44, 44)
                 .addComponent(jLabel6)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 44, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 52, Short.MAX_VALUE)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel1)
                     .addComponent(comboBoxSiswa, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
@@ -136,16 +172,24 @@ public class peminjaman extends javax.swing.JFrame {
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(comboBoxBuku, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel5))
-                .addGap(30, 30, 30)
-                .addComponent(jLabel7)
-                .addGap(26, 26, 26)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(22, 22, 22)
+                        .addComponent(jLabel7))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(18, 18, 18)
+                        .addComponent(tanggal, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addGap(20, 20, 20)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel3)
                     .addComponent(txtLamaPinjam, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel2))
-                .addGap(30, 30, 30)
-                .addComponent(jLabel4)
-                .addGap(33, 33, 33)
+                .addGap(26, 26, 26)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel4)
+                    .addComponent(dipinjam)
+                    .addComponent(dikembalikan))
+                .addGap(29, 29, 29)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jButton1)
                     .addComponent(jButton2))
@@ -170,6 +214,8 @@ public class peminjaman extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_comboBoxSiswaActionPerformed
 
+    
+    
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         try {
             model.simpan(this);
@@ -177,6 +223,18 @@ public class peminjaman extends javax.swing.JFrame {
             Logger.getLogger(peminjaman.class.getName()).log(Level.SEVERE, null, ex);
         }
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void dipinjamActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_dipinjamActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_dipinjamActionPerformed
+
+    private void dikembalikanActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_dikembalikanActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_dikembalikanActionPerformed
+
+    private void comboBoxBukuActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_comboBoxBukuActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_comboBoxBukuActionPerformed
 
     /**
      * @param args the command line arguments
@@ -194,15 +252,11 @@ public class peminjaman extends javax.swing.JFrame {
                     break;
                 }
             }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(peminjaman.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(peminjaman.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(peminjaman.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(peminjaman.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        
         //</editor-fold>
 
         /* Create and display the form */
@@ -221,8 +275,15 @@ public class peminjaman extends javax.swing.JFrame {
     public javax.swing.JComboBox<String> comboBoxBuku;
     public javax.swing.JComboBox<String> comboBoxSiswa;
     private org.jdatepicker.impl.DateComponentFormatter dateComponentFormatter1;
+    private org.jdatepicker.impl.DateComponentFormatter dateComponentFormatter2;
+    private org.jdatepicker.impl.DateComponentFormatter dateComponentFormatter3;
+    public javax.swing.JRadioButton dikembalikan;
+    public javax.swing.JRadioButton dipinjam;
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
+    private org.jdatepicker.JDateComponentFactory jDateComponentFactory1;
+    private org.jdatepicker.util.JDatePickerUtil jDatePickerUtil1;
+    private org.jdatepicker.util.JDatePickerUtil jDatePickerUtil2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
@@ -230,7 +291,9 @@ public class peminjaman extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
     private javax.swing.JLabel jLabel7;
-    private javax.swing.JPanel jPanel1;
+    public javax.swing.JPanel jPanel1;
+    public com.toedter.calendar.JDateChooser tanggal;
     public javax.swing.JTextField txtLamaPinjam;
+    private org.jdatepicker.impl.UtilCalendarModel utilCalendarModel1;
     // End of variables declaration//GEN-END:variables
 }
