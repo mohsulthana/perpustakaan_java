11ab52a5098cec8c1d60f41f3707bed2a61e3a29
KeikoATR72 <58618886+KeikoATR72@users.noreply.github.com>
2019-12-09 15:08:09 +0700

    Fixed (reuploaded)

--- src/main/java/model/modelPeminjaman.java
+++ src/main/java/model/modelPeminjaman.java
@@ -12,6 +12,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import javax.swing.JOptionPane;
+import javax.swing.JTextField;
 import koneksi.koneksi;
 import views.dashboard;
 import views.admin_dashboard;
@@ -79,47 +80,63 @@ public final class modelPeminjaman implements controllerPeminjaman {
         }
     }
 
-public void siswa(peminjaman pinjam) throws SQLException {
-        koneksi conn = new koneksi();
-        Connection koneksi = conn.getKoneksi();
-        Statement st = null;
-        ResultSet rs = null;
+    public void siswa(tabelSiswa siswa, peminjaman pinjam) throws SQLException {
         
         try{
-            String sql = "SELECT * FROM siswa";
-            rs = st.executeQuery(sql);
-                while(rs.next())
-                {
-                    pinjam.comboboxsiswa.addItem(rs.getString(2));
+                String sql = "SELECT * FROM siswa ORDER BY nama";
+                Statement st = con.getKoneksi().createStatement();
+                ResultSet rs = st.executeQuery(sql);
+                
+                while (rs.next()){
+                    pinjam.comboBoxSiswa.addItem(rs.getString("nama"));
                 }
+                
+                rs.last();
+                int jumlahdata = rs.getRow();
+                rs.first();
         }
-        catch(SQLException e){
-            JOptionPane.showMessageDialog(null, "Error");
+        catch (SQLException ex) {
+             JOptionPane.showMessageDialog(null,ex);
         }
-        finally{
-            try{
-                st.close();
-                rs.close();
-                con.close();
-        }catch(SQLException e){
-            JOptionPane.showMessageDialog(null, "ERROR CLOSE");
-        }
+    }   
     
+    public void buku(peminjaman pinjam) throws SQLException {
+        
+        try{
+                String sql = "SELECT * FROM buku ORDER BY kode_buku";
+                Statement st = con.getKoneksi().createStatement();
+                ResultSet rs = st.executeQuery(sql);
+                
+                while (rs.next()){
+                    pinjam.comboBoxBuku.addItem(rs.getString("judul_buku"));
+                }
+                
+                rs.last();
+                int jumlahdata = rs.getRow();
+                rs.first();
         }
+        catch (SQLException ex) {
+             JOptionPane.showMessageDialog(null,ex);
+    }
 }
+    
     @Override
     public void simpan(peminjaman pinjam) throws SQLException {
-        koneksi conn = new koneksi();
-        Connection koneksi = conn.getKoneksi();
-        String sql  = "INSERT INTO peminjaman (id_pinjam, tgl_pinjam, nama_siswa, buku, lama_pinjam, status) VALUES (?, ?, ?, ?, ?, ?)";
-        try (PreparedStatement prepare = koneksi.prepareStatement(sql)) {
+        String sql  = "INSERT INTO peminjaman (id_pinjam, tgl_pinjam, nama_siswa, lama_pinjam, status) VALUES (?, ?, ?, ?, ?)";
+        try (PreparedStatement prepare = konek.prepareStatement(sql)) {
             prepare.setString(1, null);
 //            prepare.setString(5, buku.txtPengarang.getText());
-            prepare.setString(3, (String) pinjam.comboboxsiswa.getSelectedItem());
-            prepare.setString(4, (String) pinjam.comboBoxBuku.getSelectedItem());
-            prepare.setString(5, pinjam.txtLamaPinjam.getText());
+            prepare.setString(2,((JTextField) pinjam.tanggal.getDateEditor().getUiComponent()).getText());
+            prepare.setString(3, (String) pinjam.comboBoxSiswa.getSelectedItem());
+//            prepare.setString(4, (String) pinjam.comboBoxBuku.getSelectedItem());
+            prepare.setString(4, pinjam.txtLamaPinjam.getText());
 //            prepare.setString(6, pinjam.st.getText());
-            
+            if (pinjam.dipinjam.isSelected())
+            {
+                prepare.setString(5, "Dipinjam");
+            } else if (pinjam.dikembalikan.isSelected()){
+                prepare.setString(5, "Dikembalikan");
+            }            
             prepare.execute();
 
         } catch (SQLException e) {
